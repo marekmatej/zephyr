2ef7ac0cea30b900ec44bd4c68c6f41dae9fd4ed
Marek Matej <marek.matej@espressif.com>
2024-09-11 12:08:56 +0200

    samples: driver: ipm: update ipm_esp32 remote sample
    Signed-off-by: Marek Matej <marek.matej@espressif.com>

--- samples/drivers/ipm/ipm_esp32/remote/src/main.c
+++ samples/drivers/ipm/ipm_esp32/remote/src/main.c
@@ -9,35 +9,35 @@
 #include <zephyr/drivers/ipm.h>
 #include <zephyr/device.h>
 
-//static const struct device *ipm_dev;
-//static const char fake_resp[] = {"APP_CPU: This is a response"};
-//struct k_sem sync;
-//
-//static void ipm_receive_callback(const struct device *ipmdev, void *user_data,
-//			       uint32_t id, volatile void *data)
-//{
-//	k_sem_give(&sync);
-//}
+static const struct device *ipm_dev;
+static const char fake_resp[] = {"APP_CPU: This is a response"};
+struct k_sem sync;
+
+static void ipm_receive_callback(const struct device *ipmdev, void *user_data,
+			       uint32_t id, volatile void *data)
+{
+	k_sem_give(&sync);
+}
 
 int main(void)
 {
-//	k_sem_init(&sync, 0, 1);
-//
-//	ipm_dev = DEVICE_DT_GET(DT_NODELABEL(ipm0));
-//	if (!ipm_dev) {
-//		printk("Failed to get IPM device.\n\r");
-//		return 0;
-//	}
-//
-//	ipm_register_callback(ipm_dev, ipm_receive_callback, NULL);
+	k_sem_init(&sync, 0, 1);
+
+	ipm_dev = DEVICE_DT_GET(DT_NODELABEL(ipm0));
+	if (!ipm_dev) {
+		printk("Failed to get IPM device.\n\r");
+		return 0;
+	}
+
+	ipm_register_callback(ipm_dev, ipm_receive_callback, NULL);
 
 	int i = 0;
 
 	while (1) {
 		k_msleep(1000);
-		printk("kokot%d\n", i++);
-//		k_sem_take(&sync, K_FOREVER);
-//		ipm_send(ipm_dev, -1, sizeof(fake_resp), &fake_resp, sizeof(fake_resp));
+//		printk("kokot%d\n", i++);
+		k_sem_take(&sync, K_FOREVER);
+		ipm_send(ipm_dev, -1, sizeof(fake_resp), &fake_resp, sizeof(fake_resp));
 	}
 	return 0;
 }

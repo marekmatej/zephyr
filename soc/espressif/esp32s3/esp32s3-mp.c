d47b68c00365e9a693621a5ad53f467723b1fabb
Marek Matej <marek.matej@espressif.com>
2024-09-28 00:38:02 +0200

    soc: espressif: common: AMP slightly work! WIP
    Signed-off-by: Marek Matej <marek.matej@espressif.com>

--- soc/espressif/esp32s3/esp32s3-mp.c
+++ soc/espressif/esp32s3/esp32s3-mp.c
@@ -15,11 +15,10 @@
 #include <esp_cpu.h>
 #include <zephyr/drivers/interrupt_controller/intc_esp32.h>
 
-//#include "esp32s3/rom/uart.h"
 #include "esp_mcuboot_image.h"
 //#include "esp_image_loader.h"
 #include "esp_memory_utils.h"
-//#include "bootloader_flash_priv.h"
+
 
 void smp_log(const char *msg)
 {
@@ -31,6 +30,7 @@ void smp_log(const char *msg)
 }
 
 #ifdef CONFIG_SOC_ENABLE_APPCPU
+
 void esp_appcpu_start(void *entry_point)
 {
 	esp_cpu_unstall(1);
@@ -44,21 +44,30 @@ void esp_appcpu_start(void *entry_point)
 
 	esp_rom_ets_set_appcpu_boot_addr((void *)entry_point);
 
-	ets_delay_us(50000);
+//	ets_delay_us(50000);
 
 	smp_log("ESP32S3: CPU1 start sequence complete");
+	ets_printf("ESP32S3: CPU1 start sequence complete");
 }
 
 static int load_segment(uint32_t src_addr, uint32_t src_len, uint32_t dst_addr)
 {
 	const uint32_t *data = (const uint32_t *)sys_mmap(src_addr, src_len);
+
 	if (!data) {
-		ets_printf("%s: Bootloader mmap failed", __func__);
+		ets_printf("%s: mmap failed", __func__);
 		return -1;
 	}
-	memcpy((void *)dst_addr, data, src_len);
+
+	volatile uint32_t *dst = (volatile uint32_t *)dst_addr;
+
+	for (int i = 0; i < src_len / 4; i++) {
+		dst[i] = data[i];
+	}
+
 	sys_munmap(data);
-	ets_printf("%s: %x ->(%dB)-> %x\n", __func__, src_addr, src_len, dst_addr);
+
+	smp_log("ESP32S3: loaded segment"); //CPU1 start sequence complete");
 	return 0;
 }
 
@@ -77,35 +86,32 @@ int esp_appcpu_image_load(unsigned int hdr_offset, unsigned int *entry_addr)
 		return -1;
 	}
 
-	ets_printf("Loading appcpu image from flash, area id: %d, offset: 0x%x, size: %d kB\n",
-	fa_id, img_off + hdr_offset, fa_size/1024);
+	ets_printf("Loading appcpu image from flash, area id: %d, offset: 0x%x, hdr.offset: 0x%x, size: %d kB\n",
+	fa_id, img_off, hdr_offset, fa_size/1024);
 
-	const uint32_t *data = (const uint32_t *)sys_mmap(img_off + hdr_offset,
-				sizeof(esp_image_load_header_t));
-
+	uint32_t mcuboot_header[8] = {0};
 	esp_image_load_header_t img_header = {0};
-	memcpy((void *)&img_header, data, sizeof(esp_image_load_header_t));
+
+	const uint32_t *data = (const uint32_t *)sys_mmap(img_off, 0x40);  // sizeof(esp_image_load_header_t));
+
+	memcpy((void *)&mcuboot_header, data, sizeof(mcuboot_header));
+	memcpy((void *)&img_header, data + hdr_offset/sizeof(uint32_t), sizeof(esp_image_load_header_t));
+
 	sys_munmap(data);
 
-	hexdump("esp_image", &img_header, sizeof(esp_image_load_header_t));
+	hexdump("appcpu MCUboot hdr:", &mcuboot_header, sizeof(mcuboot_header));
+	hexdump("appcpu ESPmeta hdr:", &img_header, sizeof(esp_image_load_header_t));
 
 	if (img_header.header_magic == ESP_LOAD_HEADER_MAGIC) {
 		ets_printf("MCUboot image format - header magic found\n");
 	} else if ((img_header.header_magic & 0xff) == 0xE9) {
 		ets_printf("ESP image format - header magic found\n");
+		ets_printf("but it is not supported yet\n");
+		abort();
 	} else {
 		ets_printf("Unknown or empty image detected. Aborting!\n");
 		abort();
 	}
-
-//	ets_printf("hdr.magic = 0x%x\n", img_header.header_magic);
-//	ets_printf("hdr.entry = 0x%x\n", img_header.entry_addr);
-//	ets_printf("hdr.iram_dest = 0x%x\n", img_header.iram_dest_addr);
-//	ets_printf("hdr.iram_src  = 0x%x\n", img_header.iram_flash_offset + img_off);
-//	ets_printf("hdr.iram_size = 0x%x\n", img_header.iram_size);
-//	ets_printf("hdr.dram_dest = 0x%x\n", img_header.dram_dest_addr);
-//	ets_printf("hdr.dram_src  = 0x%x\n", img_header.dram_flash_offset + img_off);
-//	ets_printf("hdr.dram_size = 0x%x\n", img_header.dram_size);
 
 	if (!esp_ptr_in_iram((void *)img_header.iram_dest_addr) ||
 	    !esp_ptr_in_iram((void *)(img_header.iram_dest_addr + img_header.iram_size))) {
@@ -126,15 +132,16 @@ int esp_appcpu_image_load(unsigned int hdr_offset, unsigned int *entry_addr)
 	}
 
 	ets_printf("Application start=%xh\n", img_header.entry_addr);
-	ets_printf("IRAM segment: paddr=%08xh, vaddr=%08xh, size=%05xh (%6d) load\n",
+	ets_printf("iram segment: paddr=%08xh, vaddr=%08xh, size=%05xh (%6d) load\n",
 	(img_off + img_header.iram_flash_offset), img_header.iram_dest_addr,
 	img_header.iram_size, img_header.iram_size);
 
 	load_segment(img_off + img_header.iram_flash_offset, img_header.iram_size, img_header.iram_dest_addr);
 
-	ets_printf("DRAM segment: paddr=%08xh, vaddr=%08xh, size=%05xh (%6d) load\n",
+	ets_printf("dram segment: paddr=%08xh, vaddr=%08xh, size=%05xh (%6d) load\n",
 	(img_off + img_header.dram_flash_offset), img_header.dram_dest_addr,
 	img_header.dram_size, img_header.dram_size);
+
 	load_segment(img_off + img_header.dram_flash_offset, img_header.dram_size, img_header.dram_dest_addr);
 
 	uart_tx_wait_idle(0);
@@ -155,6 +162,7 @@ void esp_appcpu_image_start(unsigned int hdr_offset)
 		return;
 	}
 
+	/* Input image meta header, output appcpu entry point */
 	esp_appcpu_image_load(hdr_offset, &entry_addr);
 
 	ets_printf("Starting APPCPU with entry address 0x%x\n", entry_addr);
